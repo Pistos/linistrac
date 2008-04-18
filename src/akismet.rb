77d1eeb44ee379a73a014b4995b3ed7a307aac8b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-04-18 14:14:02 -0400

    If no Akismet key, assume nothing is spam.

--- src/akismet.rb
+++ src/akismet.rb
@@ -2,30 +2,34 @@ class Akismet
   # Generic Akismet POST, used by all Akismet operations.
   def self.post( action, hash, request )
     akismet_key = Configuration.get( 'akismet_key' )
-    http = SimpleHttp.new "#{akismet_key}.rest.akismet.com/1.1/#{action}"
-    http.request_headers[ 'User-Agent' ] = "LinisTrac/#{LinisTrac::VERSION} | LinisTrac/#{LinisTrac::VERSION}"
-    post_params = {
-      'blog' => 'http://linis.purepistos.net',
-      'user_ip' => request.env[ 'REMOTE_ADDR' ],
-      'user_agent' => request.env[ 'HTTP_USER_AGENT' ],
-      'referrer' => request.env[ 'HTTP_REFERER' ],
-      # and all request.env
-      #'permalink' => '',
-      #'comment_author_email' => '',
-      #'comment_author_url' => '',
-    }.merge( hash )
-    
-    post_params.each_key do |key|
-      if post_params[ key ].nil?
-        raise "Akismet error: post_params[ #{key.inspect} ] is nil"
+    if akismet_key.nil? or akismet_key.strip.empty?
+      'false'
+    else
+      http = SimpleHttp.new "#{akismet_key}.rest.akismet.com/1.1/#{action}"
+      http.request_headers[ 'User-Agent' ] = "LinisTrac/#{LinisTrac::VERSION} | LinisTrac/#{LinisTrac::VERSION}"
+      post_params = {
+        'blog' => 'http://linis.purepistos.net',
+        'user_ip' => request.env[ 'REMOTE_ADDR' ],
+        'user_agent' => request.env[ 'HTTP_USER_AGENT' ],
+        'referrer' => request.env[ 'HTTP_REFERER' ],
+        # and all request.env
+        #'permalink' => '',
+        #'comment_author_email' => '',
+        #'comment_author_url' => '',
+      }.merge( hash )
+      
+      post_params.each_key do |key|
+        if post_params[ key ].nil?
+          raise "Akismet error: post_params[ #{key.inspect} ] is nil"
+        end
       end
-    end
-    
-    begin
-      http.post( post_params )
-    rescue Exception => e
-      Ramaze::Log.error "Error when posting to Akismet.  post_params:\n#{post_params.inspect}"
-      raise e
+      
+      begin
+        http.post( post_params )
+      rescue Exception => e
+        Ramaze::Log.error "Error when posting to Akismet.  post_params:\n#{post_params.inspect}"
+        raise e
+      end
     end
   end
   
