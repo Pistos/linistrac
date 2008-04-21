ba0d6d17fab62e68a14c3717de20ce6c2fa567bb
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-04-21 11:21:40 -0400

    Now using display: none in CSS instead of hide() in JS. DRY-ed up some of the message box CSS.

--- public/js/page.js
+++ public/js/page.js
@@ -2,8 +2,8 @@ $( document ).ready( function() {
     $( 'table.sortable' ).tablesorter(
         { widgets: ['zebra'] }
     );
-    $( '.error,.notice,.success' ).hide().fadeIn( 2000 );
+    $( '.error,.notice,.success' ).fadeIn( 2000 );
     
     $.scrollTo( '.new', 500 );
-    $( '.new' ).hide().fadeIn( 2000 );
+    $( '.new' ).fadeIn( 2000 );
 } );
