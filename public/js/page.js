024236bd9b668dde4f3bf4710cec18580f7555ed
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-04-21 11:01:49 -0400

    Fading in message boxen.

--- public/js/page.js
+++ public/js/page.js
@@ -2,4 +2,6 @@ $( document ).ready( function() {
     $( 'table.sortable' ).tablesorter(
         { widgets: ['zebra'] }
     );
+    
+    $( '.error,.notice,.success' ).hide().fadeIn( 2000 );
 } );
