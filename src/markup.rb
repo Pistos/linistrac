6faa89ea187232576a59770fdd3a45e10d585793
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-04-26 17:49:45 -0400

    nil check bug fix.

--- src/markup.rb
+++ src/markup.rb
@@ -1,6 +1,6 @@
 class String
   def marked_up
-    s = gsub!( /ticket(?: #?|:)(\d+)/i, "[\\0](/ticket/view/\\1)" )
+    s = self.gsub( /ticket(?: #?|:)(\d+)/i, "[\\0](/ticket/view/\\1)" )
     RedCloth.new( s ).to_html
   end
 end
