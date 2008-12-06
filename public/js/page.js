e59887c361de1d7f0e88992784daa8074770ccd6
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-12-05 23:10:21 -0500

    Change sort order of Priority column.

--- public/js/page.js
+++ public/js/page.js
@@ -3,14 +3,14 @@ var show_child_groups = function() {
     $(this).siblings( '.group-selector' ).hide();
     $( '[name="group_id"]' ).removeAttr( 'name' );
     $(this).attr( 'name', 'group_id' );
-    
+
     // Deselect child selection
     $( '#children-of-' + id + ' > select option' ).eq( 0 ).attr( 'selected', 'true' );
     $( '#children-of-' + id + ' .group-selector' ).hide();
-    
+
     $( '#children-of-' + id ).show();
 };
-    
+
 $( document ).ready( function() {
     var table;
     table = $( 'table.sortable:not(#ticket-list)' );
@@ -31,17 +31,17 @@ $( document ).ready( function() {
                 cssAsc: 'sort-up',
                 cssDesc: 'sort-down',
                 sortList: [
-                    [ 6, 0 ], [ 3, 1 ], [ 7, 0 ], [ 5, 1 ]
+                    [ 6, 0 ], [ 3, 0 ], [ 7, 0 ], [ 5, 1 ]
                 ]
             }
         );
     }
-    
+
     $( '#filter-toggle' ).click( function() {
         $( '#filter' ).toggle( 'fast' );
         return false;
     } );
-        
+
     $( '.toggle-all' ).click( function() {
         var checked = this.checked;
         $(this).siblings().each( function() {
@@ -53,46 +53,46 @@ $( document ).ready( function() {
             $(this).siblings( '.toggle-all' ).get(0).checked = false;
         }
     } );
-    
+
     $( '.group-selector > select' ).click( show_child_groups );
     $( '.group-selector > select' ).change( show_child_groups );
-    
+
     // ---------------------
-    
+
     $( '.error,.notice,.success' ).fadeIn( 2000 );
-    
+
     if( $( '.new' ).size() > 0 ) {
         $.scrollTo( '.new', 500 );
         $( '.new' ).fadeIn( 2000 );
     }
-    
+
     $( '#comment-editor, #description-editor' ).markItUp( {
         previewParserPath: "/markdown_preview", // path to your Markdown parser
         onShiftEnter: { keepDefault:false,    openWith:'\n\n' },
-    markupSet: [         
-        {name:'First Level Heading', key:"1", placeHolder:'Your title here...', openWith:"\n", 
+    markupSet: [
+        {name:'First Level Heading', key:"1", placeHolder:'Your title here...', openWith:"\n",
          closeWith:function(h) {
             heading1 = '';
             n = $.trim(h.selection||h.placeHolder).length;
             for(i = 0; i < n; i++)    {
-                heading1 += '=';    
+                heading1 += '=';
             }
             return '\n'+heading1+'\n';
         }},
-        {name:'Second Level Heading', key:"2", placeHolder:'Your title here...', openWith:"\n", 
+        {name:'Second Level Heading', key:"2", placeHolder:'Your title here...', openWith:"\n",
          closeWith:function(h) {
             heading2 = '';
             n = $.trim(h.selection||h.placeHolder).length;
             for(i = 0; i < n; i++)    {
-                heading2 += '-';    
+                heading2 += '-';
             }
             return '\n'+heading2+'\n';
         }},
         {name:'Heading 3', key:"3", openWith:'### ', placeHolder:'Your title here...' },
         {name:'Heading 4', key:"4", openWith:'#### ', placeHolder:'Your title here...' },
         {name:'Heading 5', key:"5", openWith:'##### ', placeHolder:'Your title here...' },
-        {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },                            
-        {separator:'---------------' },        
+        {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },
+        {separator:'---------------' },
         {name:'Bold', key:"B", openWith:'**', closeWith:'**'},
         {name:'Italic', key:"I", openWith:'_', closeWith:'_'},
         {separator:'---------------' },
@@ -103,9 +103,9 @@ $( document ).ready( function() {
         {separator:'---------------' },
         {name:'Picture', key:"P", replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
         {name:'Link', key:"L", openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
-        {separator:'---------------'},    
+        {separator:'---------------'},
         {name:'Quotes', openWith:'> '},
-        {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},                                                                    
+        {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
         {separator:'---------------'},
         {name:'Preview', call:'preview', className:"preview"}
     ]
